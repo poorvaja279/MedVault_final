e015c2717398b4b59e4e877d29f58f4e4f272875
poorvaja279 <poorvaja.shankaran2023@vitstudent.ac.in>
2025-11-05 12:07:19 +0530

    Delete medvault-backend/models/Approval.js

--- medvault-backend/models/Approval.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const ApprovalSchema = new mongoose.Schema({
-  patientId: { type: String, required: true },
-  doctorId: { type: String, required: true },
-  approvedDate: { type: Date, default: Date.now }
-});
-
-module.exports = mongoose.model("Approval", ApprovalSchema);
